2c5f0b8d1f522422d0d695769f65d194207afc02
Dmytro Skrypka <2346677tyu@gmail.com>
2023-01-25 21:03:19 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nimble API Service
+
+## Installation
+
+Python3 must be already installed
+
+```shell
+git clone https://github.com/skrypka-dmytro/Nimble_API_Service.git
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 main.py
+```
+
+## Features
+
+### Uploading file to AWS S3
+
+## DEMO
+
+![Website Interface](demo1.png)
+
+### Chose file
+
+![Chose Interface](demo2.png)
+
+### Upload success
+
+![Upload success](demo3.png)
+
+### AWS S3
+
+![AWS S3](demo4.png)
--- main.py
+++ main.py
@@ -1,13 +1,67 @@
-from fastapi import FastAPI
+"""
+Run this file
+"""
+import magic
+from uuid import uuid4
 
-app = FastAPI()
+import uvicorn
+from starlette import status
+from starlette.responses import HTMLResponse
+
+from settings import config
+from settings.app import app, templates
+from settings.file_types import SUPPORTED_FILE_TYPE
+from src.helpers import s3_upload
+
+from fastapi import UploadFile, HTTPException, Request
 
 
 @app.get("/")
-async def test():
-    return {"message": "Hello World, test message"}
+def test():
+    """ Nimble AWS S3 """
+    return "Nimble AWS S3 Upload and Download file"
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/files", response_class=HTMLResponse)
+def file_upload(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("upload_file.html", context)
+
+
+@app.post("/upload")
+async def upload(file: UploadFile | None = None):
+    """
+    Upload main function
+    :param file: File
+    :return: Status
+    """
+    if not file:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="File not found!"
+        )
+
+    contents = await file.read()
+    file_type = magic.from_buffer(buffer=contents, mime=True)
+
+    if file_type not in SUPPORTED_FILE_TYPE:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Unsupported file type: {file_type}. Supported file are {SUPPORTED_FILE_TYPE}"
+        )
+
+    await s3_upload(contents=contents, key=f"{uuid4()}.{SUPPORTED_FILE_TYPE[file_type]}")
+
+    return {
+        "status": "ok",
+        "info": "Upload file success"
+    }
+
+
+def start():
+    """ Starts webserver with uvicorn """
+    uvicorn.run(app, host=config.API_HOST, port=int(config.API_PORT))
+
+
+if __name__ == '__main__':
+    start()
